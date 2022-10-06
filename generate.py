5c8bc68010fd7135d1549df7b6f8f17182a9cfa7
SubwayMan <tyche4490@gmail.com>
2022-10-05 21:08:09 -0700

    tree generation that is maybe working

--- generate.py
+++ generate.py
@@ -2,9 +2,13 @@ import random
 import sys
 import os
 
+MAXIMUM_NODES = 200
+
+# load words
 with open("words.txt", "r") as f:
     words = f.read().strip().split("\n")
 
+# get/set seed
 seed = input("Enter seed: ")
 if not seed:
     seed = random.randrange(sys.maxsize)
@@ -14,4 +18,26 @@ if isinstance(seed, str) and seed.isdigit():
 
 random.seed(seed)
 print(f"Initialized generator with seed {seed}")
-print(random.sample(words, 6))
+
+# build graph
+n = random.randrange(MAXIMUM_NODES)
+prufer = dict((i, 1) for i in range(n))
+pruferlist = []
+tree = dict((i, []) for i in range(n))
+
+for i in range(n-2):
+    x = random.randrange(n)
+    prufer[x] += 1
+    pruferlist.append(x)
+
+
+for el in pruferlist:
+    for i in range(n):
+        if prufer[i] == 1:
+            tree[el].append(i)
+            tree[i].append(el)
+            prufer[i] -= 1
+            prufer[el] -= 1
+            break
+
+
